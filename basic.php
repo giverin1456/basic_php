40d5cfe82a35ee3ff281958c17c9377c76e249c8
giverin1456 <give.taiki1456@gmail.com>
2022-06-15 22:31:01 +0900

    php基礎2

---
+++ basic.php
@@ -0,0 +1,38 @@
+<?php
+    $x = 1071;
+
+    if ($x % 3 ==0 && $x % 7==0){
+      echo "xは3の倍数かつ7の倍数です。";
+    } elseif ($x % 7 == 0){
+      echo "xは3の倍数ですが7の倍数ではありません。";
+    } elseif ($x % 3 == 0){
+      echo "xは7の倍数ですが3の倍数ではありません。";
+    } else {
+      echo "xは7の倍数でも3の倍数でもありません。";
+    }
+  ?>
+
+<?php
+    $num = 9;
+    $remainder = $num % 3;
+
+    // switch文を用いてください
+    switch ($remainder){
+      case 0:
+      echo "大吉です。";
+      break;
+      
+      case 1:
+      echo "中吉です。";
+      break;
+        
+      case 2:
+      echo "小吉です。";
+      break;
+          
+      default:
+      echo "凶です。";
+      break;
+    }
+  
+  ?>
--- index.php
+++ index.php
@@ -21,3 +21,20 @@ $b = 4;
     $name = 'にんじゃわんこ';
     echo "こんにちは！{$name}さん";
   ?>
+
+
+<?php
+$price = 1000;
+$taxRate = 0.08;
+echo '変数$priceの値: '.$price;
+echo '<br>';
+echo '変数$taxRateの値: '.$taxRate;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+$taxIncludedPrice = $price + $price * $taxRate;            
+echo "税込価格は{$taxIncludedPrice}円です";
+
+?>
